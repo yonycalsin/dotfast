8d8baeb557a8c2fadd7cc8fb174d8d95e598ebf7
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 15:40:13 -0500

    feat: 🎸 Main code builded as es5

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 interface More {
     [key: string]: any;
 }
-declare const dotfast: (obj: More, path: string, defaultVal?: any) => any;
+declare const dotfast: <T = any>(data: More, paths: string | More | string[], defaultVal?: any) => T;
 export default dotfast;
 export { dotfast as dtf, dotfast };
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var dotfast = function (obj, path, defaultVal) {
+var is_all_utils_1 = require("is-all-utils");
+var dotfast = function (data, paths, defaultVal) {
+    if (is_all_utils_1.isObj(paths)) {
+        var payload = is_all_utils_1.isObject(paths) ? {} : [];
+        for (var _i = 0, _a = Object.entries(paths); _i < _a.length; _i++) {
+            var _b = _a[_i], pathKey = _b[0], path = _b[1];
+            payload[pathKey] = dotfast(data, path);
+        }
+        return payload;
+    }
+    var obj = data;
     var names;
-    names = path.split('.').reverse();
+    names = paths.split('.').reverse();
     while (names.length && obj !== undefined && obj !== null) {
         obj = obj[names.pop()];
     }
