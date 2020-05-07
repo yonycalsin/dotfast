b4102056877b3e47663180855cbf19d3b582bf55
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 15:33:26 -0500

    ci: 🎡 Aggregated array system !

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
+import { isObj, isObject } from 'is-all-utils';
+
 interface More {
    [key: string]: any;
 }
-/**
+
+/**!
  * @author Yoni Calsin <helloyonicb@gmail.com>
  * @param obj More
- * @param path string
+ * @param paths string
  * @param defaultVal any | undefined
  */
-const dotfast = (obj: More, path: string, defaultVal?: any) => {
+const dotfast = <T = any>(
+   data: More,
+   paths: string | string[] | More,
+   defaultVal?: any,
+): T => {
+   // If is an object or array
+   if (isObj(paths)) {
+      const payload = isObject(paths) ? {} : [];
+      for (const [pathKey, path] of Object.entries(paths)) {
+         payload[pathKey] = dotfast(data, path);
+      }
+      return payload as T;
+   }
+
+   let obj = data;
    let names: any[];
-   names = path.split('.').reverse();
+   names = (paths as string).split('.').reverse();
    while (names.length && obj !== undefined && obj !== null) {
       obj = obj[names.pop()];
    }
@@ -18,5 +35,4 @@ const dotfast = (obj: More, path: string, defaultVal?: any) => {
 };
 
 export default dotfast;
-
 export { dotfast as dtf, dotfast };
